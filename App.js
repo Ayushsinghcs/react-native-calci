7c29b0ecaac6167abd89295d9ec08953d6b0f441
Ayush Singh <41228583+Ayushsinghcs@users.noreply.github.com>
2020-08-30 00:48:20 +0530

    working buttons added

--- App.js
+++ App.js
@@ -1,170 +1,304 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
 export default function App() {
-  const [value, setValue] = React.useState(0);
   const [operandOne, setOperandOne] = React.useState(0);
   const [operandTwo, setOperandTwo] = React.useState(0);
   const [operator, setOperator] = React.useState(null);
   const [isOperator, setOperatorBoolean] = React.useState(false);
 
-  const updateNumber = (name) => {
-    let number = isOperator ? operandTwo : operandOne;
-    let computedValue
-    switch (name) {
-      case "number-nine":
-        isOperator
-          ?
-          setOperandTwo(number * 10 + 9)
-          : setOperandOne(number * 10 + 9);
-        break;
-      case "number-eight":
-        isOperator
-          ? setOperandTwo(number * 10 + 8)
-          : setOperandOne(number * 10 + 8);
-        break;
-      case "number-seven":
-        isOperator
-          ? setOperandTwo(number * 10 + 7)
-          : setOperandOne(number * 10 + 7);
-        break;
-      case "number-six":
-        isOperator
-          ? setOperandTwo(number * 10 + 6)
-          : setOperandOne(number * 10 + 6);
-        break;
-      case "number-five":
-        isOperator
-          ? setOperandTwo(number * 10 + 5)
-          : setOperandOne(number * 10 + 5);
-        break;
-      case "number-four":
-        isOperator
-          ? setOperandTwo(number * 10 + 4)
-          : setOperandOne(number * 10 + 4);
-        break;
-      case "number-three":
-        isOperator
-          ? setOperandTwo(number * 10 + 3)
-          : setOperandOne(number * 10 + 3);
-        break;
-      case "number-two":
-        isOperator
-          ? setOperandTwo(number * 10 + 2)
-          : setOperandOne(number * 10 + 2);
-        break;
-      case "number-one":
-        isOperator
-          ? setOperandTwo(number * 10 + 1)
-          : setOperandOne(number * 10 + 1);
-        break;
-      case "number-zero":
-        isOperator
-          ? setOperandTwo(number * 10)
-          : setOperandOne(number * 10);
-        break;
-    }
-  };
   const resetValue = () => {
     setOperatorBoolean(false);
-    setOperandOne(0)
-    setOperandTwo(0)
-    setOperator(null)
-    setValue(0)
-  }
+    setOperandOne(0);
+    setOperandTwo(0);
+    setOperator(null);
+  };
+  const removeLast = () => {
+    if (!isOperator) {
+      setOperandOne(parseInt(operandOne / 10));
+    } else if (isOperator && operandTwo === 0) {
+      setOperator(null);
+      setOperatorBoolean(false);
+      setOperandTwo(0);
+    } else if (isOperator) {
+      setOperandTwo(parseInt(operandOne / 10));
+    }
+  };
+  const calculateTotal = () => {
+    const computedValue =
+      operandTwo === 0 && !Number.isInteger(operandOne)
+        ? operandOne
+        : eval(operandOne + operator + operandTwo);
+    setOperatorBoolean(false);
+    setOperandTwo(0);
+    setOperator(null);
+    setOperandOne(computedValue);
+  };
 
   const updateOperator = (operatorName) => {
-
     setOperatorBoolean(true);
     if (operatorName === "operator-divide") {
-
       setOperator("/");
-
     } else if (operatorName === "operator-multiply") {
-      setOperator("x");
-
+      setOperator("*");
     } else if (operatorName === "operator-substract") {
-
-
       setOperator("-");
-
     } else if (operatorName === "operator-add") {
-
-
       setOperator("+");
-
+    } else if (operatorName === "operator-percent") {
+      setOperator("%");
     }
   };
 
-  // if (operandTwo !== 0 && isOperator) {
-  //   let computedValue = operandOne + operator + operandTwo;
-  //   setValue(computedValue);
-  //   console.log(computedValue)
-  // }
-  console.log(value)
-  console.log(operandTwo + 'op')
+  const updateNumber = (name) => {
+    let number = isOperator ? operandTwo : operandOne;
+    let isNumber = Number.isInteger(number);
+    switch (name) {
+      case "number-nine":
+        if (isOperator) {
+          isNumber
+            ? setOperandTwo(number * 10 + 9)
+            : setOperandTwo(`${number}9`);
+        } else {
+          isNumber
+            ? setOperandOne(number * 10 + 9)
+            : setOperandOne(`${number}9`);
+        }
+        break;
+      case "number-eight":
+        if (isOperator) {
+          isNumber
+            ? setOperandTwo(number * 10 + 8)
+            : setOperandTwo(`${number}8`);
+        } else {
+          isNumber
+            ? setOperandOne(number * 10 + 8)
+            : setOperandOne(`${number}8`);
+        }
+        break;
+      case "number-seven":
+        if (isOperator) {
+          isNumber
+            ? setOperandTwo(number * 10 + 7)
+            : setOperandTwo(`${number}7`);
+        } else {
+          isNumber
+            ? setOperandOne(number * 10 + 7)
+            : setOperandOne(`${number}7`);
+        }
+        break;
+      case "number-six":
+        if (isOperator) {
+          isNumber
+            ? setOperandTwo(number * 10 + 6)
+            : setOperandTwo(`${number}6`);
+        } else {
+          isNumber
+            ? setOperandOne(number * 10 + 6)
+            : setOperandOne(`${number}6`);
+        }
+        break;
+      case "number-five":
+        if (isOperator) {
+          isNumber
+            ? setOperandTwo(number * 10 + 5)
+            : setOperandTwo(`${number}5`);
+        } else {
+          isNumber
+            ? setOperandOne(number * 10 + 5)
+            : setOperandOne(`${number}5`);
+        }
+        break;
+      case "number-four":
+        if (isOperator) {
+          isNumber
+            ? setOperandTwo(number * 10 + 4)
+            : setOperandTwo(`${number}4`);
+        } else {
+          isNumber
+            ? setOperandOne(number * 10 + 4)
+            : setOperandOne(`${number}4`);
+        }
+        break;
+      case "number-three":
+        if (isOperator) {
+          isNumber
+            ? setOperandTwo(number * 10 + 3)
+            : setOperandTwo(`${number}3`);
+        } else {
+          isNumber
+            ? setOperandOne(number * 10 + 3)
+            : setOperandOne(`${number}3`);
+        }
+        break;
+      case "number-two":
+        if (isOperator) {
+          isNumber
+            ? setOperandTwo(number * 10 + 2)
+            : setOperandTwo(`${number}2`);
+        } else {
+          isNumber
+            ? setOperandOne(number * 10 + 2)
+            : setOperandOne(`${number}2`);
+        }
+        break;
+      case "number-one":
+        if (isOperator) {
+          isNumber
+            ? setOperandTwo(number * 10 + 1)
+            : setOperandTwo(`${number}1`);
+        } else {
+          isNumber
+            ? setOperandOne(number * 10 + 1)
+            : setOperandOne(`${number}1`);
+        }
+        break;
+      case "number-zero":
+        if (isOperator) {
+          isNumber ? setOperandTwo(number * 10) : setOperandTwo(`${number}0`);
+        } else {
+          isNumber ? setOperandOne(number * 10) : setOperandOne(`${number}0`);
+        }
+        break;
+    }
+  };
+  const updateDot = () => {
+    let number = isOperator ? operandTwo : operandOne;
+    if (isOperator) {
+      Number.isInteger(number)
+        ? setOperandTwo(`${number}.`)
+        : setOperandTwo(number);
+    } else {
+      Number.isInteger(number)
+        ? setOperandOne(`${number}.`)
+        : setOperandOne(number);
+    }
+  };
   return (
     <View style={styles.container}>
       <View style={styles.outerElement}>
         <TouchableOpacity style={styles.content}>
           <Text style={styles.contentText}>
-            {operandOne} {operator}
+            {operandOne}
+            {operator}
             {isOperator && operandTwo !== 0 ? operandTwo : null}
           </Text>
         </TouchableOpacity>
       </View>
       <View style={styles.box}>
-        <Text style={styles.elementBorder} onPress={resetValue}>
-          AC
-        </Text>
-        <Text style={styles.elementBorder}>C</Text>
-        <Text style={styles.elementBorder}>%</Text>
-        <Text
-          style={styles.elementBorder}
+        <TouchableOpacity onPress={resetValue} style={styles.numberBox}>
+          <Text style={styles.elementBorder}>AC</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={removeLast} style={styles.numberBox}>
+          <Text style={styles.elementBorder}>C</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => updateOperator("operator-percent")}
+          style={styles.numberBox}
+        >
+          <Text style={styles.elementBorder}>%</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
           onPress={() => updateOperator("operator-divide")}
+          style={styles.numberBox}
         >
-          /
-        </Text>
+          <Text style={styles.elementBorder}>/</Text>
+        </TouchableOpacity>
       </View>
       <View style={styles.box}>
-        <Text
-          style={styles.element}
+        <TouchableOpacity
           onPress={(e) => updateNumber("number-seven")}
+          style={styles.numberBox}
         >
-          7
-        </Text>
-        <Text style={styles.element} onPress={(e) => updateNumber("number-eight")}>8</Text>
-        <Text
-          style={styles.element}
+          <Text style={styles.element}>7</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={(e) => updateNumber("number-eight")}
+          style={styles.numberBox}
+        >
+          <Text style={styles.element}>8</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
           onPress={(e) => updateNumber("number-nine")}
+          style={styles.numberBox}
         >
-          9
-        </Text>
-        <Text
-          style={styles.elementBorder}
+          <Text style={styles.element}>9</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
           onPress={() => updateOperator("operator-multiply")}
+          style={styles.numberBox}
         >
-          x
-        </Text>
+          <Text style={styles.elementBorder}>x</Text>
+        </TouchableOpacity>
       </View>
       <View style={styles.box}>
-        <Text style={styles.element} onPress={(e) => updateNumber("number-four")}>4</Text>
-        <Text style={styles.element} onPress={(e) => updateNumber("number-five")}>5</Text>
-        <Text style={styles.element} onPress={(e) => updateNumber("number-six")}>6</Text>
-        <Text style={styles.elementBorder} onPress={() => updateOperator("operator-substract")}>-</Text>
+        <TouchableOpacity
+          onPress={(e) => updateNumber("number-four")}
+          style={styles.numberBox}
+        >
+          <Text style={styles.element}>4</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={(e) => updateNumber("number-five")}
+          style={styles.numberBox}
+        >
+          <Text style={styles.element}>5</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={(e) => updateNumber("number-six")}
+          style={styles.numberBox}
+        >
+          <Text style={styles.element}>6</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => updateOperator("operator-substract")}
+          style={styles.numberBox}
+        >
+          <Text style={styles.elementBorder}>-</Text>
+        </TouchableOpacity>
       </View>
       <View style={styles.box}>
-        <Text style={styles.element} onPress={(e) => updateNumber("number-one")}>1</Text>
-        <Text style={styles.element} onPress={(e) => updateNumber("number-two")}>2</Text>
-        <Text style={styles.element} onPress={(e) => updateNumber("number-three")}>3</Text>
-        <Text style={styles.elementBorder} onPress={() => updateOperator("operator-add")}>+</Text>
+        <TouchableOpacity
+          onPress={(e) => updateNumber("number-one")}
+          style={styles.numberBox}
+        >
+          <Text style={styles.element}>1</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={(e) => updateNumber("number-two")}
+          style={styles.numberBox}
+        >
+          <Text style={styles.element}>2</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={(e) => updateNumber("number-three")}
+          style={styles.numberBox}
+        >
+          <Text style={styles.element}>3</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => updateOperator("operator-add")}
+          style={styles.numberBox}
+        >
+          <Text style={styles.elementBorder}>+</Text>
+        </TouchableOpacity>
       </View>
       <View style={styles.box}>
-        <Text style={styles.element}></Text>
-        <Text style={styles.element} onPress={(e) => updateNumber("number-zero")}>0</Text>
-        <Text style={styles.element}>.</Text>
-        <Text style={styles.elementBorder}>=</Text>
+        <TouchableOpacity style={styles.numberBox}>
+          <Text style={styles.element}></Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={(e) => updateNumber("number-zero")}
+          style={styles.numberBox}
+        >
+          <Text style={styles.element}>0</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.numberBox} onPress={updateDot}>
+          <Text style={styles.element}>.</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={calculateTotal} style={styles.numberBox}>
+          <Text style={styles.elementBorder}>=</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -174,7 +308,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "black",
-    padding: 20,
   },
   outerElement: {
     height: "40%",
@@ -183,6 +316,8 @@ const styles = StyleSheet.create({
     flex: 1,
     opacity: 0.7,
     shadowOpacity: 0.5,
+    alignContent: "flex-end",
+    justifyContent: "flex-end",
   },
   box: {
     borderColor: 1,
@@ -190,7 +325,7 @@ const styles = StyleSheet.create({
     opacity: 1,
     justifyContent: "space-between",
     flexDirection: "row",
-    margin: 10,
+    marginVertical: 10,
     alignItems: "center",
     height: 50,
   },
@@ -205,14 +340,18 @@ const styles = StyleSheet.create({
     fontSize: 40,
   },
   content: {
-    fontSize: 40,
-    justifyContent: "center",
-    alignItems: "center",
-    bottom: 0,
+    bottom: 2,
+    right: 0,
     position: "absolute",
   },
   contentText: {
     color: "#FFFFFF",
     fontSize: 40,
+  },
+  numberBox: {
+    justifyContent: "center",
+    alignItems: "center",
+    height: 50,
+    width: "25%",
   },
 });
